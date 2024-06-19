d457e0607099619896664643a2d2f8fcfbd151b7
DrHrast <petar.huljek@gmail.com>
2024-06-19 13:29:31 +0200

    GUI first commit.

---
+++ GUI/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace GUI
+{
+    public partial class MainPage : ContentPage
+    {
+        int rows, columns = 5;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            DrawGrid(rows, columns);
+        }
+
+        private void DrawGrid(int rows, int columns)
+        {
+            for (int i = 0; i < int.Max(rows, columns); i++)
+            {
+                
+            }
+        }
+    }
+}
